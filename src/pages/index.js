0b8a4ca4b8cb6d40aa5a0545e16704b87ed5d574
fastsaleinc <germainandresdev@gmail.com>
2024-11-25 09:46:11 -0300

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+// src/pages/index.js
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-gray-500">Hello, World!</h1>
+    </div>
+  );
+};
+
+export default HomePage;
